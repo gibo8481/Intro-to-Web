427b9670476fbefe9a7b35f15101218e43fdad81
Gia <gibo8481@colorado.edu>
2024-04-10 22:25:55 -0600

    ica 12

--- image-carousel/gallery-start/wa11.js
+++ image-carousel/gallery-start/wa11.js
@@ -5,53 +5,50 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imgs = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
-
+const filenames = ['pic1.jpg', 'pic2.jpg','pic3.jpg','pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 const alts = {
-    'pic1.jpg' : 'close up of eye',
-    'pic2.jpg' : 'wave rock',
-    'pic3.jpg' : 'purple flowers',
-    'pic4.jpg' : 'egyptian tomb',
-    'pic5.jpg' : 'moth on leaf'
+  'pic1.jpg' : 'close up of eye',
+  'pic2.jpg' : 'wave rock',
+  'pic3.jpg' : 'purple flowers',
+  'pic4.jpg' : 'egyptian tomb',
+  'pic5.jpg' : 'moth on leaf'
 }
-
 /* Looping through images */
-for(const i of imgs){
 
-// construct src path and alt text
-const src = `images/${i}`;
-const nalt = alts[i]; // matching each images filename to its corresponding text
+for(const i of filenames){
+  // construct src ath and alt text
+  const nalt = alts[i];
+  const nsrc = `images/${i}`;
 
-// create new img element for the thumbnail
-const newImage = document.createElement('img'); 
-// set source and alt text of thumbnail img
-newImage.setAttribute('src', src);
-newImage.setAttribute('alt', nalt);
-// append thumbnail img to thumbar container
-thumbBar.appendChild(newImage);
+  // update image for the thumbnail
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', nsrc);
+  newImage.setAttribute('alt', nalt);
+  thumbBar.appendChild(newImage);
 
-// change displayed image when thumbnail is clicked
-newImage.addEventListener('click', () => {
-    displayedImage.setAttribute('src', src);
+  // change displayed image when thumbnail is clicked
+  newImage.addEventListener('click', () =>{
+    displayedImage.setAttribute('src', nsrc);
     displayedImage.setAttribute('alt', nalt);
-})
+  })
 
 }
+
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', ()=>{
+  const btnClass = btn.getAttribute('class'); // current class of btn
 
-btn.addEventListener('click', () =>{
-    const btnClass = btn.getAttribute('class');
-    // curr class of btn
-    if (btnClass === 'dark') {
-        btn.setAttribute('class','light');
-        btn.textContent = 'Lighten';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-      } else {
-        btn.setAttribute('class','dark');
-        btn.textContent = 'Darken';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-      }
+  if(btnClass === 'dark'){
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  }
+  else{
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)'
+  }
 })
